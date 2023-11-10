8c2422dcb09c968647df57b354fbb16306f109b0
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-11-10 21:23:01 +0300

    **

---
+++ index4.py
@@ -0,0 +1,9 @@
+# Python program to check if the input number is even
+
+# Ask the user to enter an integer
+number = int(input("Please enter an integer number: "))
+
+# Check if the number is divisible by 2
+if number % 2 == 0:
+    # If it is, print "HiEven"
+    print("HiEven")
